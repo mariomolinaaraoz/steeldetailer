69d3c18ac6a9e69f1293c7bc22c1658aa613144d
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2024-01-30 21:06:12 -0300

    add svg file

--- src/app/(dashboard)/(routes)/dashboard/(table)/(routes)/table/components/Upn.tsx
+++ src/app/(dashboard)/(routes)/dashboard/(table)/(routes)/table/components/Upn.tsx
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 import {
   Table,
@@ -48,9 +49,8 @@ const Upn: React.FC<Props> = ({ setFilteredLength }) => {
 
   return (
     <Suspense fallback={<h2>Loading UPN...</h2>}>
-      <div
-        id="subtitle"
-        className="px-6 w-full flex flex-row justify-between sm:px-6 lg:px-20 xl:px-20"
+     <div
+        className="w-full flex flex-row justify-between sm:px-6 lg:px-20 xl:px-20"
       >
         <h1 className="w-1/3 hidden sm:hidden lg:flex xl:flex">
           <u>List of profile:</u>
@@ -63,29 +63,57 @@ const Upn: React.FC<Props> = ({ setFilteredLength }) => {
           />
         </div>
       </div>
-      <div className="flex flex-row px-20">
-        <div className="w-3/4">
-          <Table>            
+      <div className="w-full flex flex-row gap-4 justify-around sm:px-6 lg:px-20 xl:px-20">
+        <Button variant="secondary">UPN</Button>
+        <Button variant="secondary">PNL</Button>
+        <Button variant="secondary">Perfil C</Button>
+        <Button variant="secondary">Chapa Lisa</Button>
+        <Button variant="secondary">UPN</Button>
+        <Button variant="secondary">PNL</Button>
+        <Button variant="secondary">Perfil C</Button>
+        <Button variant="secondary">Chapa Lisa</Button>
+      </div>
+      <div className="flex flex-col-reverse justify-center sm:flex-col-reverse lg:flex-row xl:flex-row">
+        <div className="w-full sm:w-full lg:w-2/3 xl:w-2/3">
+          <Table>
             <TableHeader>
               <TableRow>
-                <TableHead>UPN</TableHead>
-                <TableHead className="w-fit px-2 text-center">h</TableHead>
-                <TableHead className="w-fit px-2 text-center">b</TableHead>
-                <TableHead className="w-fit px-2 text-center">e</TableHead>
+                <TableHead>UPN</TableHead>                
+                <TableHead className="w-fit px-2 text-center">
+                  h
+                </TableHead>
+                <TableHead className="w-fit px-2 text-center">
+                  b
+                </TableHead>
+                <TableHead className="w-fit px-2 text-center">
+                  e
+                </TableHead>
                 <TableHead className="w-fit px-2 text-center">e1=r</TableHead>
                 <TableHead className="w-fit px-2 text-center">r1</TableHead>
                 <TableHead className="w-fit px-2 text-center">h1</TableHead>
                 <TableHead className="w-fit px-2 text-center">A(cm2)</TableHead>
-                <TableHead className="w-fit px-2 text-center">P(Kg/m)</TableHead>
-                <TableHead className="w-fit px-2 text-center">Ix(cm4)</TableHead>
-                <TableHead className="w-fit px-2 text-center">Wx(cm3)</TableHead>
+                <TableHead className="w-fit px-2 text-center">
+                  P(Kg/m)
+                </TableHead>
+                <TableHead className="w-fit px-2 text-center">
+                  Ix(cm4)
+                </TableHead>
+                <TableHead className="w-fit px-2 text-center">
+                  Wx(cm3)
+                </TableHead>
                 <TableHead className="w-fit px-2 text-center">ix(cm)</TableHead>
-                <TableHead className="w-fit px-2 text-center">Iy(cm4)</TableHead>
-                <TableHead className="w-fit px-2 text-center">Wy(cm3)</TableHead>
+                <TableHead className="w-fit px-2 text-center">
+                  Iy(cm4)
+                </TableHead>
+                <TableHead className="w-fit px-2 text-center">
+                  Wy(cm3)
+                </TableHead>
                 <TableHead className="w-fit px-2 text-center">iy(cm)</TableHead>
                 <TableHead className="w-fit px-2 text-center">m2/m</TableHead>
                 <TableHead className="w-fit px-2 text-center">UPN</TableHead>
               </TableRow>
+              
+              
             </TableHeader>
             <TableBody>
               {filtered.map((data, index) => (
@@ -93,43 +121,78 @@ const Upn: React.FC<Props> = ({ setFilteredLength }) => {
                   <TableCell className="font-bold tracking-widest text-right">
                     {data.UPN}
                   </TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.h}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.b}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.e}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.e1}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.r1}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.h1}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.a}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.p}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.Ix}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.wx}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.ix}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.Iy}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.Wy}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.iy}</TableCell>
-                  <TableCell className="w-fit px-2 text-right">{data.m2m}</TableCell>
+                  
+                    <TableCell className="w-fit px-2 text-right" scope="col">
+                      {data.h}
+                    </TableCell>
+                    <TableCell className="w-fit px-2 text-right" scope="col">
+                      {data.b}
+                    </TableCell>
+                    <TableCell className="w-fit px-2 text-right" scope="col">
+                      {data.e}
+                    </TableCell>
+                  
+
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.e1}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.r1}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.h1}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.a}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.p}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.Ix}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.wx}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.ix}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.Iy}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.Wy}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.iy}
+                  </TableCell>
+                  <TableCell className="w-fit px-2 text-right">
+                    {data.m2m}
+                  </TableCell>
                   <TableCell className="font-bold tracking-widest text-right">
                     {data.UPN}
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
-            {/* <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total</TableCell>
-              <TableCell className="text-right">$2,500.00</TableCell>
-            </TableRow>
-          </TableFooter> */}
           </Table>
         </div>
-        <div className="w-3/12 flex flex-col justify-center space-y-8">
-          <Image className="h-1/2 w-full scale-150" src="/UPN220.svg" width={400} height={400} alt="UPN"/>          
-          <div className="h-2/6 w-full grid grid-col gap-4 justify-center">
-              <h1>A = Area de la sección</h1>
-              <h1>I = Momento de Inercia</h1>
-              <h1>W = Módulo resistente</h1>
-              <h1>i = √(I/A) = Radio de giro</h1>
-              <h1>u = Sup. Lateral x mts.</h1>           
+        <div id="perfil"
+          className="overflow-hidden sm:w-full lg:w-3/12 xl:w-3/12 flex flex-col justify-center"
+        >
+          <Image className="sm:h-[300px] lg:h-[400px] xl:h-[400px] scale-100 sm:scale-100 lg:scale-150 xl:scale-150"
+            src="/UPN220.svg"
+            width={400}
+            height={400}
+            alt="Perfil UPN"
+          />
+          <div className="h-8 sm:h-8 lg:h-16 xl:h-16"></div>
+          <div className="hidden grid grid-col gap-4 justify-center sm:hidden lg:grid xl:grid">
+            <h1>A = área de la sección</h1>
+            <h1>I = momento de inercia</h1>
+            <h1>W = módulo resistente</h1>
+            <h1>i = √(I/A) = radio de giro</h1>
+            <h1>u = sup. lateral x mts.</h1>
           </div>
         </div>
       </div>
